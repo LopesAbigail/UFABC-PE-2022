8bd4618b612d660c2b03e1ea6904a489b53fd534
Abi <contatoabigail_lopes@outlook.com>
2022-03-08 22:16:00 -0300

    Update McLourin series method and add an example

--- Semana02/MaclourinSeriesLog.c
+++ Semana02/MaclourinSeriesLog.c
@@ -1,55 +1,35 @@
 #include <stdio.h>
 
-int pow (int numero, int expoente) {
-
-	if(expoente == 0) {
-       return 1;
-	} else if(expoente == 1) {
-	    return numero;
-	} else {
-	    return numero*pow(numero,expoente-1);
-	}
-}
-
-int multi (int x, int serie) {
-    int valor = 0;
-
-    for (int i = 1; i <= serie; i++) {
-        int power = pow(x, i);
-        valor += i * power;
-    }
-
-    return valor;
-}
-
+// output: (1/1)*x - (1/2)*x*x + (1/3)*x*x*x - (1/4)*x*x*x*x + ... + (1/n)*x (n[times])
 double logaritmo (double x, int n) {
-    // Codigo da funcao aqui
-    double valor;
-
-    for (int i = 1; i <= n; i++) {
-        double power = pow(x, i+1);
-        valor += (1.0/x) - (1.0/(x+ (double) i)) * power;
+    x -= 1;
+    // x fora do intervalo em que a somatória converge
+    if (x < -1.0 || x > 1.0) {
+        return 0.0;
     }
 
-    return valor;
+    double resultado = x;
+    double expoente = x;
+
+    for (int i = 2; i <= n; i++) {
+        expoente *= x;
+        if (i % 2 == 0) {
+            resultado -= expoente * 1.0/i;
+        } else {
+            resultado += expoente * 1.0/i;
+        }
+    }
+
+    return resultado;
+
 }
 
 void main() {
-    double x = 0;
-    int n = 0;
-    //printf("Insira um numero para calcular o log: ");
-    //scanf("%lf", &x);
-    //printf("\nValor de n: ");
-    //scanf("%d", &n);
-    x = 0.5;
-    n = 10;
+    double x = 0.5;
+    int n = 10;
+    // output:  -0.69306
 
-    double logX = 0;
-    logX = logaritmo(x, n);
+    double log = logaritmo(x, n);
+    printf("%lf", log);
 
-    //printf("\nO valor do log eh: %lf", x);
-    //int power = pow(2, 5);
-    //printf("Potencia de 2: %d", power);
-    int valor = multi(2, 3);
-    printf("\nO valor da multi eh: %d", valor);
 }
