283913d430c0246e2365b49a3a298d9086b86f24
Abi <contatoabigail_lopes@outlook.com>
2022-03-29 11:56:19 -0300

    Add debubgs printfs

--- Semana04-Ponteiros/PointersOperations.c
+++ Semana04-Ponteiros/PointersOperations.c
@@ -34,7 +34,11 @@ void operacao_int(int a, int b, char op, int* res) {
     res = &resultado_inteiro;
     *res = resultado_inteiro;
 
-    printf("\nPonteiro: \n%d", *res);
+    printf("\nPonteiro com D: %d", *res);
+    printf("\nPonteiro com P: %p", *res);
+
+    printf("\nValor inteiro com D: %d", res);
+    printf("\nValor inteiro com P: %p", res);
 
 }
 
