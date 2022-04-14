f3ebcb96b8177ebfaad768069e7236e2cc428299
Abi <contatoabigail_lopes@outlook.com>
2022-04-14 11:04:43 -0300

    Add comments

--- Semana04-Ponteiros/SortParameters.c
+++ Semana04-Ponteiros/SortParameters.c
@@ -1,24 +1,56 @@
-void ordena(int *a, int *b, int *c) {
+#include <stdio.h>
+
+void inverte(int *x, int *y) {
+
+    int temp;
+    temp = *x;
+    *x = *y;
+    *y = temp;
+}
+
+void ordena(int *a, int *b, int *c)
+{
     // Receber 3 ponteiros
     // comparar os valores para os quais cada ponteiro aponta
     // Trocar valores de apontamento, de modo que A aponte para o menor, B para o mediano e C para o maior dos valores
-
-    int maior, medio, menor = 0;
-    // Acha o menor, comparando os inteiros apontados por a, b e c
-    if ( (a < b) && (b < c) ) {
-        menor = a;
-    } else if ( (a >= b) && (b < c) ) {
-        menor = b;
-    } else {
-        menor = c;
-
+    int temp = 0;
+    if (*a > *c) {
+        temp = 0;
+        temp = *a;
+        *a = *c;
+        *c = temp;
     }
 
-    *a = menor;
-    *b = medio;
-    *c = maior;
+    if (*a > *b) {
+        temp = 0;
+        temp = *a;
+        *a = *b;
+        *b = temp;
+    }
+
+    if (*b > *c) {
+        temp = 0;
+        temp = *b;
+        *b = *c;
+        *c = temp;
+    }
+
+
 }
 
 int main() {
+    void inverte(int *a, int *b);
+    void ordena(int *a, int *b, int *c);
+    int num_a, num_b, num_c;
+
+    num_a = 50;
+    num_b = 90;
+    num_c = 15;
+
+    // Inverte
+    // passar como parametro o endereco de memoria dos ponteiros
+    printf("BEFORE %d %d %d", num_a, num_b, num_c);
+    ordena(&num_a, &num_b, &num_c);
+    printf("\nAFTER %d %d %d", num_a, num_b, num_c);
 
 }
